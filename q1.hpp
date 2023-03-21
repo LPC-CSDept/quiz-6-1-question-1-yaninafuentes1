16e367f7a902c157da29499527d24d830c17ebbd
yaninafuentes1 <115580036+yaninafuentes1@users.noreply.github.com>
2023-03-21 10:11:59 -0700

    Update q1.hpp

--- q1.hpp
+++ q1.hpp
@@ -28,21 +28,3 @@ void getinput(float& f1, float& f2) {
     cout << "Enter second float: ";
     cin >> f2;
 }
-
-int main() {
-    int n1, n2;
-    float f1, f2;
-    
-    cout << "Enter two integers: " << endl;
-    getinput(n1, n2);
-    swapTwoValues(n1, n2);
-    cout << "Swapped integers: " << n1 << " " << n2 << endl;
-    
-    cout << "Enter two floats: " << endl;
-    getinput(f1, f2);
-    swapTwoValues(f1, f2);
-    cout << fixed << setprecision(2);
-    cout << "Swapped floats: " << f1 << " " << f2 << endl;
-    
-    return 0;
-}
