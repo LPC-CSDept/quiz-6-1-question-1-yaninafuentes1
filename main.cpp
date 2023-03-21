8738fb34fc78b83832b48dbe071cd54ef32a0d14
yaninafuentes1 <115580036+yaninafuentes1@users.noreply.github.com>
2023-03-21 10:15:09 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include "q1.hpp"
 
-int main() {
-    int n1, n2;
-    float f1, f2;
-    
-    cout << "Enter two integers: " << endl;
-    getinput(n1, n2);
-    swapTwoValues(n1, n2);
-    cout << "Swapped integers: " << n1 << " " << n2 << endl;
-    
-    cout << "Enter two floats: " << endl;
-    getinput(f1, f2);
-    swapTwoValues(f1, f2);
-    cout << fixed << setprecision(2);
-    cout << "Swapped floats: " << f1 << " " << f2 << endl;
-    
+int main()
+{
+	int num1, num2;
+	float fnum1, fnum2;
+	getinput(num1, num2);
+	cout << num1 << " " << num2 << endl;
+	swapTwoValues(num1, num2);
+	cout << num1 << " " << num2 << endl;
+
+	// -- Call overloaded function with float parameters
+	getinput(fnum1, fnum2);
+	cout << fixed << setprecision(2);
+	cout << fnum1 << " " << fnum2 << endl;
+	swapTwoValues(fnum1, fnum2);
+	cout << num1 << " " << num2 << endl;
 }
