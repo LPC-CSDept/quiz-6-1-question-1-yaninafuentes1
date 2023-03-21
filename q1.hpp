82c3b1b66ec70ca043c10820d1fce89808ffd870
yaninafuentes1 <115580036+yaninafuentes1@users.noreply.github.com>
2023-03-21 10:07:42 -0700

    Update q1.hpp

--- q1.hpp
+++ q1.hpp
@@ -1,11 +1,48 @@
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void swapTwoValues(int n1, int n2) {
+    int temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void swapTwoValues(float f1, float f2) {
+    float temp = f1;
+    f1 = f2;
+    f2 = temp;
+}
+
+void getinput(int& n1, int& n2) {
+    cout << "Enter first integer: ";
+    cin >> n1;
+    cout << "Enter second integer: ";
+    cin >> n2;
+}
+
+void getinput(float& f1, float& f2) {
+    cout << "Enter first float: ";
+    cin >> f1;
+    cout << "Enter second float: ";
+    cin >> f2;
+}
+
+int main() {
+    int n1, n2;
+    float f1, f2;
+    
+    cout << "Enter two integers: " << endl;
+    getinput(n1, n2);
+    swapTwoValues(n1, n2);
+    cout << "Swapped integers: " << n1 << " " << n2 << endl;
+    
+    cout << "Enter two floats: " << endl;
+    getinput(f1, f2);
+    swapTwoValues(f1, f2);
+    cout << fixed << setprecision(2);
+    cout << "Swapped floats: " << f1 << " " << f2 << endl;
+    
+    return 0;
+}
